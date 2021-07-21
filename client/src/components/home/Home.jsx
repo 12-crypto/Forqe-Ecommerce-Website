81d654aac62ec90777d962d4766f8efbbc56c28d
12-crypto <dhruvrathee2000@gmail.com>
2021-07-21 15:46:52 +0530

    v3
    v3

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,13 +1,26 @@
-import NavBar from "./NavBar"
-import Banner from "./Banner"
+import { Box, makeStyles } from '@material-ui/core';
 
+import NavBar from "./NavBar";
+import Banner from "./Banner";
+import Slide from "./Slide";
+
+const useStyle = makeStyles({
+    component: {
+        padding: 10,
+        background: '#F2F2F2'
+    }
+})
 
 const Home = () => {
+    const classes = useStyle();
 
     return (
         <div>
             <NavBar />
+            <Box className = {classes.component}>
             <Banner />
+            <Slide />
+            </Box>
 
         </div>
 
